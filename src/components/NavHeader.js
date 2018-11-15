ab6c1317c09559d5f85e94064bc1ad37a6db6019
SparksFullStack <csparks@antioch.edu>
2018-11-15 15:26:15 -0800

    conditional styles for the active page added--dropdown menu in progress

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -7,7 +7,11 @@ import {
     Collapse,
     Nav,
     NavItem,
-    NavLink
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
  } from 'reactstrap';
 
 export default class NavHeader extends Component {
@@ -22,32 +26,52 @@ export default class NavHeader extends Component {
 
 
     render() {
+        const { collapsed, currentPage } = this.state;
+
         return (
             <Navbar className="p-0" color="dark" dark expand="sm">
             <NavbarBrand href="/" className="mr-auto">Blogden</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse isOpen={!this.state.collapsed} navbar>
+            <Collapse isOpen={!collapsed} navbar>
               <Nav className="mr-auto" navbar>
-                <NavItem className={this.state.currentPage === "dashboard" ? "px-2 active" : "px-2"}>
+                <NavItem className={currentPage === "dashboard" ? "px-2 active" : "px-2"}>
                     <NavLink>
                         Dashboard
                     </NavLink>
                 </NavItem>
-                <NavItem className="px-2">
+                <NavItem className={currentPage === "posts" ? "px-2 active" : "px-2"}>
                     <NavLink>
                         Posts
                     </NavLink>
                 </NavItem>
-                <NavItem className="px-2">
+                <NavItem className={currentPage === "categories" ? "px-2 active" : "px-2"}>
                     <NavLink>
                         Categories
                     </NavLink>
                 </NavItem>
-                <NavItem className="px-2">
+                <NavItem className={currentPage === "users" ? "px-2 active" : "px-2"}>
                     <NavLink>
                         Users
                     </NavLink>
                 </NavItem>
+              </Nav>
+
+              <Nav className="ml-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem dark>
+                            <a href="#">
+                                <i className="fas fa-user"></i> Welcome!
+                            </a>
+                        </DropdownItem>
+                        <DropdownItem>
+
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
               </Nav>
             </Collapse>
           </Navbar>
